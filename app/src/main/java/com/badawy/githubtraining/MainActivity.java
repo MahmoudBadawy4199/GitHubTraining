bb2fb57384e1e1e8b021d0eb2c21295ddc550aa6
yussuf44 <55205582+yussuf44@users.noreply.github.com>
2019-09-12 14:21:28 +0200

    Update MainActivity.java

--- app/src/main/java/com/badawy/githubtraining/MainActivity.java
+++ app/src/main/java/com/badawy/githubtraining/MainActivity.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String hello = "HelloWorld!";
+text findviewbyid(R.id.txt);
+    
     }
 }
